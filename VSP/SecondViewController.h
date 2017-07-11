7ccecb6380be479d93528f8efcdfd7f9066bdde0
FighterRay <fighterray@GX-MacBook-Pro.local>
2017-07-11 15:43:45 +0800

    Initial Commit

---
+++ VSP/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  VSP
+//
+//  Created by FighterRay on 2017/7/11.
+//  Copyright © 2017年 FighterRay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ VSP/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  VSP
+//
+//  Created by FighterRay on 2017/7/11.
+//  Copyright © 2017年 FighterRay. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
